40575c549529a4d6ab40685c273f26433159425e
mjlosch <Martin.Losch@awi.de>
2024-10-28 11:41:36 +0100

    fine tuning of parameters
    - background diffusivity and viscosity set to zero because TKE model
    takes care of that
    - remove redundant forcing files
    - add atmospheric pressure in data.exf
    - cleanup

--- MITgcm_config/input/plotit.py
+++ MITgcm_config/input/plotit.py
@@ -5,7 +5,7 @@ from myutils import *
 from xmitgcm import open_mdsdataset
 
 import datetime as dt
-refdate = dt.datetime(2004,2,3)
+refdate = dt.datetime(2004,2,8)
 
 myrun = '/Users/mlosch/mlosch/ANTXXI-3/MITgcm_config/run'
 ds = open_mdsdataset(myrun,prefix = ['diags3D','diags2D','GGL90diags'],
