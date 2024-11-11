b02af60aa978ef38c21b82eb51083bd13c053bb2
mjlosch <Martin.Losch@awi.de>
2024-11-11 12:36:05 +0100

    add cartopy map projections

--- MITgcm_config/input/plotit.py
+++ MITgcm_config/input/plotit.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import cmocean.cm as cmo
+import cartopy.crs as ccrs
 
 from xmitgcm import open_mdsdataset
 
@@ -58,16 +59,20 @@ t0 = readfield(os.path.join(bdir,'input/theta.init'),
 s0 = readfield(os.path.join(bdir,'input/salt.init'),
                [nz,ny,nx],'float64')
 
-fig, axs = plt.subplots(2,3,figsize=(12,8),
-                        sharex=True,sharey=True,layout='constrained')
-
+proj=ccrs.Mercator()
 kz = 2
 hm = []
 myslice = slice(-10,-1)
 #myslice = slice(0,10)
 k=0
 theta_kwargs = {'norm': colors.Normalize(vmin=3.4,vmax=6),
-                'cmap': cmo.thermal}
+                'cmap': cmo.thermal, 'transform': ccrs.PlateCarree()}
+
+fig, axs = plt.subplots(2,3,figsize=(11,8),
+                        sharex=True,sharey=True,
+                        layout='constrained',
+                        subplot_kw={'projection': proj})
+
 hm.append(axs[0,0].pcolormesh(ds[k].XG,ds[k].YG,t0[kz,:,:],
                               **theta_kwargs))
 axs[0,0].set_title('initial conditions')
@@ -77,19 +82,43 @@ for k, ax in enumerate(axs[0,1:]):
     ax.set_title(names[k])
 
 salt_kwargs = {'norm': colors.Normalize(vmin=33.8,vmax=33.93),
-               'cmap': cmo.haline}
+               'cmap': cmo.haline, 'transform': ccrs.PlateCarree()}
 hm.append(axs[1,0].pcolormesh(ds[k].XG,ds[k].YG,s0[kz,:,:],
                              **salt_kwargs))
-axs[1,0].set_title('initial conditions')
 for k, ax in enumerate(axs[1,1:]):
     salt=ds[k].SALT.isel(time=myslice,Z=kz).mean(dim='time')
     hm.append(ax.pcolormesh(ds[k].XG,ds[k].YG,salt, **salt_kwargs))
 
-plt.colorbar(hm[0],ax=axs[0,:],orientation='vertical',
-             label=r'theta / $^\circ$C')
-plt.colorbar(hm[2],ax=axs[1,:],orientation='vertical',
-             label='salinity')
 
-plt.suptitle('time average over last ten days, at Z = %f m'%(ds[0].Z[kz]))
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+lon_formatter = LongitudeFormatter(dms=True,auto_hide=True)
+lat_formatter = LatitudeFormatter(dms=True,auto_hide=True)
+for ax in axs[:,0]:
+    ax.set_yticks([-50,-49],crs=ccrs.PlateCarree())
+    ax.set_yticks(np.linspace(-51,-48,10)[2:-3],minor=True,
+                  crs=ccrs.PlateCarree())
+    ax.yaxis.set_major_formatter(lat_formatter)
+    ax.yaxis.set_minor_formatter(lat_formatter)
+
+for ax in axs[1,:]:
+    ax.set_xticks([2,3],crs=ccrs.PlateCarree())
+    ax.set_xticks(np.linspace(1,4,10)[2:-2],minor=True,
+                  crs=ccrs.PlateCarree())
+    ax.xaxis.set_major_formatter(lon_formatter)
+    ax.xaxis.set_minor_formatter(lon_formatter)
+
+for ax in axs.ravel():
+    gl = ax.gridlines(draw_labels=False,
+                      xlocs = [2,3],
+                      ylocs = [-50,-49],
+                      crs=ccrs.PlateCarree()) #linetype = '--')
+
+plt.colorbar(hm[0],ax=axs[0,:],orientation='vertical',
+             label=r'theta / $^\circ$C',extend='both')
+plt.colorbar(hm[3],ax=axs[1,:],orientation='vertical',
+             label='salinity',extend='both')
+
+plt.suptitle('time average over last ten days, at Z = %f m'%(
+    ds[0].Z[kz]))
 
 plt.show()
